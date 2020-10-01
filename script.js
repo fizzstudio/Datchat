59385ca9d5c9f584d8e633dfedab88b5c872cf76
Guanxinyuan <xyguan@live.unc.edu>
2020-10-01 13:08:54 -0400

    Encap listeners in functions

--- script.js
+++ script.js
@@ -57,56 +57,87 @@ const Option = class {
     }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    const options = [];
-    const synth = window.speechSynthesis;
-    let speech = new SpeechSynthesisUtterance();
-    let option = new Option("Please request a table first", ["chart"]);
-    options.push(option);
-    let option1 = new Option("you can select the average", ['average', "mean"]);
-    options.push(option1);
-    let option2 = new Option("You can select the median", ["median"]);
-    options.push(option2);
+const options = [];
 
-    setupInterface();
+function startBot() {
+    document.addEventListener('DOMContentLoaded', () => {
 
-    function setupInterface() {
-        for (let i = 0; i < options.length; i++) {
-            speech = new SpeechSynthesisUtterance();
-            if (options[i].getState() !== states.ASKED) {
-                speech.text = options[i].getContent();
-                console.log(options[i].getHash());
-                synth.speak(speech);
+        // let speech = new SpeechSynthesisUtterance();
+        let option = new Option("Please request a table first", ["chart"]);
+        options.push(option);
+        let option1 = new Option("You can select the average", ['average', "mean"]);
+        options.push(option1);
+        let option2 = new Option("You can select the median", ["median"]);
+        options.push(option2);
+
+        setupInterface();
+        onClick();
+        onSpeechStart()
+        onSpeechResult();
+        onSpeechEnd();
+        onSpeechError();
+
+    });
+
+}
+
+startBot();
+
+
+
+function setupInterface() {
+    let silence = 0;
+    for (let i = 0; i < options.length; i++) {
+        const synth = window.speechSynthesis;
+        const speech = new SpeechSynthesisUtterance();
+        if (options[i].getState() === states.UNASKED) {
+            speech.text = options[i].getContent();
+            console.log(options[i].getHash());
+            // synth.speak(speech);
+        } else {
+            silence += 1;
+        }
+    }
+    if (silence == options.length) {
+        const synth = window.speechSynthesis;
+        const speech = new SpeechSynthesisUtterance();
+        speech.text = "Options are done. Do you want to start over?"
+        synth.speak(speech);
+
+    }
+}
+
+
+function updateState(text) {
+    let option;
+    for (let i = 0; i < options.length; i++) {
+        option = options[i];
+        if (option.getHash().includes(text)) {
+            switch (option.getState()) {
+                case states.UNASKED:
+                    option.updateState(states.ASKED);
+                    return option;
+                case states.ASKED:
+                    option.updateState(states.RECALLED);
+                    return option;
             }
         }
     }
+}
 
-
-    function updateState(text) {
-        for (let i = 0; i < options.length; i++) {
-            if (options[i].getHash().includes(text)) {
-                switch (options[i].getState()) {
-                    case states.UNASKED:
-                        options[i].updateState(states.ASKED);
-                        break;
-                    case states.ASKED:
-                        options[i].updateState(states.RECALLED);
-                        break;
-                }
-            }
-            console.log(options[i].getState());
-        }
-    }
-
-
+function onClick() {
     document.querySelector('button').addEventListener('click', () => {
         recognition.start();
     });
+}
 
+function onSpeechStart() {
     recognition.addEventListener('speechstart', () => {
         console.log('Speech has been detected.');
     });
+}
 
+function onSpeechResult() {
     recognition.addEventListener('result', (e) => {
         console.log('Result has been detected.');
 
@@ -120,69 +151,87 @@ document.addEventListener('DOMContentLoaded', () => {
 
         //   socket.emit('chat message', text);
     });
+}
 
+function onSpeechEnd() {
     recognition.addEventListener('speechend', () => {
         console.log("Speech has ended");
         recognition.stop();
     });
+}
 
+function onSpeechError() {
     recognition.addEventListener('error', (e) => {
         outputBot.textContent = 'Error: ' + e.error;
     });
+}
 
 
-    function synthVoice(text) {
-        const synth = window.speechSynthesis;
-        const speech = new SpeechSynthesisUtterance();
-        let ctx = document.getElementById('myChart');
-        let type = 'line';
+function synthVoice(text) {
+    const synth = window.speechSynthesis;
+    const speech = new SpeechSynthesisUtterance();
+    let ctx = document.getElementById('myChart');
+    let type = 'line';
+    let option = null;
 
-        speech.text = "Sorry, I did not understand that.";
+    speech.text = "Sorry, I did not understand that.";
 
-        if (/table|chart/.test(text)) {
-            updateState("chart");
-            if (/pie/.test(text)) {
-                type = 'pie'
-            }
-            if (/line/.test(text)) {
-                type = 'line'
-            }
-            if (/bar/.test(text)) {
-                type = 'bar'
-            }
-            chartIt = new ChartIt('test.csv', 'Global Average Temperature', ctx, type);
-            chart = chartIt.createChart();
-            drawn = true;
-            speech.text = 'Below is the chart you want.'
+    if (/table|chart/.test(text)) {
 
+        if (/pie/.test(text)) {
+            type = 'pie'
         }
-
-        if (/mean|average/.test(text)) {
-            updateState("average");
-            if (!drawn) {
-                speech.text = 'You need first to have a chart.'
-            } else {
-                // const average = arr => arr.reduce((sume, el) => sume + el, 0) / arr.length;
-                speech.text = 'The average of the data is ' + chartIt.mean.toFixed(2);
-                chartIt.setMeanDataset(chartIt.mean);
-                let chart_with_mean_line = chartIt.createChart();
-            }
+        if (/line/.test(text)) {
+            type = 'line'
         }
-
-        if (/median/.test(text)) {
-            updateState("median");
-            if (!drawn) {
-                speech.text = 'You need first to have a chart.'
-            } else {
-                speech.text = 'The median of the data is ' + chartIt.median.toFixed(2);
-                chartIt.setMedianDataset(chartIt.median);
-                let chart_with_median_line = chartIt.createChart();
-            }
+        if (/bar/.test(text)) {
+            type = 'bar'
         }
+        chartIt = new ChartIt('test.csv', 'Global Average Temperature', ctx, type);
+        chart = chartIt.createChart();
+        drawn = true;
+        speech.text = 'Below is the chart you want.';
+        option = updateState("chart");
+        console.log(option);
+        option.setAnswers(speech.text);
 
-        synth.speak(speech);
-        outputBot.textContent = speech.text;
-
-        setupInterface();
     }
-});
+
+    if (/mean|average/.test(text)) {
+        if (!drawn) {
+            speech.text = 'You need first to have a chart.'
+        } else {
+            // const average = arr => arr.reduce((sume, el) => sume + el, 0) / arr.length;
+            speech.text = 'The average of the data is ' + chartIt.mean.toFixed(2);
+            chartIt.setMeanDataset(chartIt.mean);
+            let chart_with_mean_line = chartIt.createChart();
+        }
+        option = updateState("average");
+        console.log(option);
+        option.setAnswers(speech.text);
+    }
+
+    if (/median/.test(text)) {
+        if (!drawn) {
+            speech.text = 'You need first to have a chart.'
+        } else {
+            speech.text = 'The median of the data is ' + chartIt.median.toFixed(2);
+            chartIt.setMedianDataset(chartIt.median);
+            let chart_with_median_line = chartIt.createChart();
+        }
+        option = updateState("median");
+        console.log(option);
+        option.setAnswers(speech.text);
+        // console.log(option.getAnswers());
+    }
+
+    if (/start/.test(text)) {
+        setupInterface();
+        speech.text = 'Ok, I just restarted myself';
+    }
+
+    synth.speak(speech);
+    outputBot.textContent = speech.text;
+
+    setupInterface();
+}
