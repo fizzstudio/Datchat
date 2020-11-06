f34e5aedde0cdb44e4ff66ba785bbc0efcddebf4
Guanxinyuan <xyguan@live.unc.edu>
2020-11-05 22:32:34 -0500

    Unified many functions to one

--- callbacks.js
+++ callbacks.js
@@ -13,6 +13,64 @@ async function makeTable() {
     response = 'below is the table'//the data of' + visual.title;
     return response;
 }
+async function statAction(stat) {
+    let response = '';
+
+    if (stat == 'chart') {
+        let canvas = document.getElementById('myChart').getContext('2d');
+        let type = 'line';
+        visual = new Visual('test.csv', 'Global Average Temperature from 1880', canvas, type);
+        chart = await visual.createChart();
+        drawn = true;
+        response = 'below is the table'//the data of' + visual.title;
+    } else {
+        if (!drawn) {
+            response = 'you need first to have a chart.';
+        } else {
+            switch (stat) {
+                case 'median':
+                    let median = ss.median(visual.ys);
+                    response = 'the median of the data is ' + median.toFixed(2);
+                    await visual.drawStatistics('median', median);
+                    break;
+                case 'average':
+                    let mean = ss.mean(visual.ys);
+                    response = 'the average of the data is ' + mean.toFixed(2);
+                    await visual.drawStatistics('mean', mean);
+                    break;
+                case 'std':
+                    let std = ss.standardDeviation(visual.ys);
+                    response = 'the standard deviation of the data is ' + std.toFixed(2);
+                    break;
+                case 'max':
+                    let max = ss.extent(visual.ys)[1];
+                    response = 'the maximum of the data is ' + max.toFixed(2);
+                    break;
+                case 'min':
+                    let min = ss.extent(visual.ys)[0];
+                    response = 'the minimum of the data is ' + min.toFixed(2);
+                    break;
+                case 'trend':
+                    let lg = ss.linearRegression(visual.pairs);
+                    if (lg.m > 0) {
+                        response = 'this data set has an overall increasing trend';
+                    } else if (lg.m = 0) {
+                        response = 'this data set remains stable';
+                    } else {
+                        response = 'this data set has an overall decreasing trend';
+                    }
+                    break;
+                case 'restart':
+                    refreshCanvas();
+                    refreshState();
+                    response = 'Ok, I just restarted myself';
+                    drawn = false;
+                    break;
+            }
+            return response;
+        }
+    }
+}
 
 async function reportAvg() {
     let response = "";
